9ea2feff6cdd95d75789a30f6b5ef95ceeb4aea0
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-27 23:48:26 +0100

    feat: solution env cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+const stringToSearch = 'RSS_'
+
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.slice(0, 4) === stringToSearch) {
+      console.log(`${key}=${value}`);
+    }
+  }
 };
 
 parseEnv();
