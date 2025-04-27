10b272013786dfaa5dcac6c09ee4c5a6a2642d56
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-27 20:29:57 +0100

    feat: create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { writeFile, existsSync } from 'node:fs';
+
+const data = "I am fresh and young"
+const file = './files/fresh.txt';
+const errorMsg = "FS operation failed"
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const exist = existsSync(file);
+  if(exist){
+    throw Error(errorMsg);
+  } else {
+    try {
+    await writeFile(file, data, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+    
+    }catch(err){
+      throw err;
+    }
+  }
 };
 
 await create();
