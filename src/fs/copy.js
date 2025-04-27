d03dd31d6d2902fb18bf99cd338455f6f8340f52
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-27 21:24:40 +0100

    feat: copy task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import {access, cp} from 'fs/promises';
+
+const folderPath = "./files"
+const copyFolder = "./files_copy"
+const errorMsg = "FS operation failed"
+
 const copy = async () => {
-    // Write your code here 
+  try{
+    await access(folderPath)
+    
+    try{
+      await access(copyFolder)
+      throw Error(errorMsg)
+    }catch(err){
+      if (err.code !== 'ENOENT') {
+        throw Error(errorMsg);
+      }
+    }
+    await cp(folderPath, copyFolder, {recursive: true})
+  }catch(e){
+   throw Error(errorMsg)
+  }
 };
 
 await copy();
