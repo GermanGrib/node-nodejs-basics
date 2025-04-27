bf99c9142bf75b2faf7b81e1ae002f625a8daf48
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-27 22:53:58 +0100

    feat: solution to delete fs task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import {access, rm} from 'fs/promises'
+
+
+const fileToDeletePath = './files/fileToRemove.txt'
+const errorMsg = 'FS operation failed'
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const isFileToDeleteExist = await access(fileToDeletePath).then(() => true).catch(() => false)
+  if (!isFileToDeleteExist) {
+    throw new Error(errorMsg)
+  }
+  try {
+    await rm(fileToDeletePath)
+  } catch (err) {
+    throw new Error(err.message)
+  }
 };
 
 await remove();
