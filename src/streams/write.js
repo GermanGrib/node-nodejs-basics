bc512bced42e46dc5b2cfc74783b2ce0d290b606
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-28 02:21:29 +0100

    feat: solution write streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import {createWriteStream} from 'fs'
+
+const toWriteFilePath = './files/fileToWrite.txt'
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const writableStream = createWriteStream(toWriteFilePath, {flags: 'a'});
+  process.stdin.pipe(writableStream);
 };
 
 await write();
