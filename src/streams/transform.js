ae2e8278363ef5f4c8e778f8a1c07e578a57267d
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-28 02:44:17 +0100

    feat: solution transform streams task & refactor paths to files with imports

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import {Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const text = chunk.toString();
+      const reversedText = text.split('').reverse().join('');
+      callback(null, reversedText);
+    }
+  });
+  
+  process.stdin
+    .pipe(reverseTransform)
+    .pipe(process.stdout);
 };
 
 await transform();
