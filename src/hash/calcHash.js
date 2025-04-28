543d085756d04f817c04b543ca3a1b1bb179efca
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-04-28 01:52:13 +0100

    feat: solution calcHash hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import {createReadStream} from 'fs'
+import {createHash} from 'crypto'
+
+const fileToReadPath = './files/fileToCalculateHashFor.txt'
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const hash = createHash('sha256')
+  const stream = createReadStream(fileToReadPath);
+  stream.on('data', chunk => hash.update(chunk));
+  stream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log(hexHash);
+  });
+  stream.on('error', error => console.error(error.message));
 };
 
 await calculateHash();
